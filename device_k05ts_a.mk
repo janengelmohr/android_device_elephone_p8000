ff25172e74f7e21834819c9406ebaf65c0a72b30
visi0nary <jan.engelmohr@mailbox.tu-dresden.de>
2015-09-10 13:28:26 +0200

    added ADDITIONAL_DEFAULT_PROPERTIES for testing purposes (thanks, carliv!)
    Change-Id: I65006a565b02558ef5e0858d056e256b79ff2926

--- device_k05ts_a.mk
+++ device_k05ts_a.mk
@@ -16,7 +16,6 @@ endif
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel \
-    $(LOCAL_PATH)/recovery/root/init.recovery.mt6735.rc:root/init.recovery.mt6735.rc \
 
 PRODUCT_COPY_FILES_OVERRIDES += \
     recovery/root/file_contexts \
@@ -32,3 +31,15 @@ PRODUCT_NAME := full_k05ts_a
 PRODUCT_DEVICE := k05ts_a
 PRODUCT_MODEL := P8000
 PRODUCT_BRAND := Elephone
+
+# Set insecure for root access and device specifics
+ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0 \
+ro.allow.mock.location=1 \
+persist.mtk.aee.aed=on \
+ro.debuggable=1 \
+ro.adb.secure=0 \
+persist.service.acm.enable=0 \
+persist.sys.usb.config=mtp \
+ro.mount.fs=EXT4 \
+ro.persist.partition.support=no \
+ro.cip.partition.support=no
