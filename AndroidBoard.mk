f2cf97337fc65594125af65dce3dede167359376
visi0nary <jan.engelmohr@mailbox.tu-dresden.de>
2015-09-10 13:44:41 +0200

    fixed typo
    Change-Id: I3069d68f8c76cadbed7b1292880bc1f0fa62321f

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,8 +1,8 @@
-+LOCAL_PATH := $(call my-dir)
-+
-+include $(CLEAR_VARS)
-+
-+ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
-+
-+# include the non-open-source counterpart to this file
-+-include vendor/alps/k05ts_a/AndroidBoardVendor.mk
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
+
+# include the non-open-source counterpart to this file
+-include vendor/alps/k05ts_a/AndroidBoardVendor.mk
