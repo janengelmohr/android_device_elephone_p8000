7afd9d5c18c55df2852809013abecc8474232915
visi0nary <jan.engelmohr@mailbox.tu-dresden.de>
2015-09-20 21:56:05 +0200

    added blob extraction scripts
    Change-Id: I12156d6425d307a370b22bc5ea341e3f5bc93acd

---
+++ extract-files.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+## usage: extract-files.sh $1 $2
+## $1 and $2 are optional
+## if $1 = unzip the files will be extracted from zip file (if $1 = anything else 'adb pull' will be used
+## $2 specifies the zip file to extract from (default = ../../../${DEVICE}_update.zip)
+
+VENDOR=alps
+DEVICE=k05ts_a
+
+BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+rm -rf $BASE/*
+
+if [ -z "$2" ]; then
+    ZIPFILE=../../../${DEVICE}_update.zip
+else
+    ZIPFILE=$2
+fi
+
+if [ "$1" = "unzip" -a ! -e $ZIPFILE ]; then
+    echo $ZIPFILE does not exist.
+else
+    for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
+        DIR=`dirname $FILE`
+	if [ ! -d $BASE/$DIR ]; then
+            mkdir -p $BASE/$DIR
+	fi
+	if [ "$1" = "unzip" ]; then
+            unzip -j -o $ZIPFILE system/$FILE -d $BASE/$DIR
+	else
+            adb pull /system/$FILE $BASE/$FILE
+	fi
+    done
+fi
+./setup-makefiles.sh
