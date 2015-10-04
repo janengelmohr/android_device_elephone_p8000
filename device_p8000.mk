1349ac682bb27d88784d715d78927f2da6a3ed23
visi0nary <jan.engelmohr@mailbox.tu-dresden.de>
2015-10-04 15:33:54 +0200

    clean up
    Change-Id: Iee5a543d3c72c57ddeb7f71e0a1451a2d17827d4

--- device_p8000.mk
+++ device_p8000.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/elephone/p8000/k05ts_a-vendor.mk)
+$(call inherit-product-if-exists, vendor/elephone/p8000/p8000-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/elephone/p8000/overlay
 
