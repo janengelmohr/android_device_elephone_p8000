722780f47e5be7fa268ea6c329e971e558e58e73
visi0nary <jan.engelmohr@mailbox.tu-dresden.de>
2015-10-10 03:41:40 +0200

    switched back to 64 bit version to match precompiled kernel
    Change-Id: Ib72be738db00aadc598c1c205f1568e1ab3d3ced

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,26 +3,22 @@ USE_CAMERA_STUB := true
 # inherit from the proprietary version
 -include vendor/elephone/p8000/BoardConfigVendor.mk
 #64 bit
-#TARGET_ARCH := arm64
-TARGET_ARCH := arm
+TARGET_ARCH := arm64
 TARGET_NO_BOOTLOADER := true
 TARGET_BOARD_PLATFORM := mt6753
-#TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 := 
-TARGET_ARCH_VARIANT := armv7-a-neon
-#TARGET_ARCH_VARIANT := armv8-a
-#TARGET_CPU_VARIANT := generic
-TARGET_CPU_VARIANT := cortex-a53
-#TARGET_CPU_CORTEX_A53 := true
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_CORTEX_A53 := true
 TARGET_BOARD_PLATFORM_GPU := Mali-T720
 
 #32 bit
-#TARGET_2ND_ARCH := arm
-#TARGET_2ND_ARCH_VARIANT := armv7-a-neon
-#TARGET_2ND_CPU_ABI := armeabi-v7a
-#TARGET_2ND_CPU_ABI2 := armeabi
-#TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
 
 TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
