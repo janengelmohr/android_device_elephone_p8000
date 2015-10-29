b0431e59162b0480fcfa0e6c1397caa1b055395f
visi0nary <jan.engelmohr@mailbox.tu-dresden.de>
2015-10-29 19:16:45 +0100

    use stock policy settings

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,7 +11,7 @@ TARGET_CPU_ABI2 :=
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_VARIANT := generic
 TARGET_CPU_CORTEX_A53 := true
-TARGET_BOARD_PLATFORM_GPU := Mali-T720
+#TARGET_BOARD_PLATFORM_GPU := Mali-T720
 
 #32 bit
 TARGET_2ND_ARCH := arm
@@ -47,9 +47,7 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 444596224
 BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x03f88000 --tags_offset 0x0df88000 
 
-TARGET_PREBUILT_KERNEL := device/elephone/p8000/prebuilt/selinux_permissive
-#use precompiled bootimage for now
-#DEVICE_BASE_BOOT_IMAGE := device/elephone/p8000/prebuilt/boot.img
+TARGET_PREBUILT_KERNEL := device/elephone/p8000/prebuilt/kernel
 BOARD_HAS_NO_SELECT_BUTTON := true
 #recovery
 #TARGET_RECOVERY_INITRC := device/elephone/p8000/recovery/init.mt6753.rc
@@ -59,7 +57,7 @@ TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/devices/platform/leds-mt65xx/leds/l
 #Mediatek flags
 BOARD_HAS_MTK_HARDWARE := true
 MTK_HARDWARE := true
-COMMON_GLOBAL_CFLAGS += -DMTK_HARDWARE -DADD_LEGACY_ACQUIRE_BUFFER_SYMBOL
+COMMON_GLOBAL_CFLAGS += -DMTK_HARDWARE 
 COMMON_GLOBAL_CPPFLAGS += -DMTK_HARDWARE
 
 #EGL settings
@@ -67,13 +65,171 @@ USE_OPENGL_RENDERER := true
 BOARD_EGL_CFG := device/elephone/p8000/egl.cfg
 
 #SELinux
-BOARD_SEPOLICY_DIRS += device/elephone/p8000/sepolicy
-BOARD_SEPOLICY_UNION += servicemanager.te \
-			healthd.te \
-			app.te \
-			system.te \
-			file_contexts \
-			device.te \
-			seapp_contexts \
-			service_contexts \
-			property_contexts
+BOARD_SEPOLICY_DIRS := device/elephone/p8000/sepolicy
+
+BOARD_SEPOLICY_UNION := \
+    app.te \
+    device.te \
+    domain.te \
+    file.te \
+    file_contexts \
+    fs_use \
+    installd.te \
+    net.te \
+    netd.te \
+    te_macros \
+    vold.te \
+    untrusted_app.te \
+    platform_app.te \
+    system_app.te \
+    zygote.te \
+    aal.te \
+    aee_core_forwarder.te \
+    akmd09911.te \
+    akmd8963.te \
+    akmd8975.te \
+    ami304d.te \
+    atcid.te \
+    atci_service.te \
+    audiocmdservice_atci.te \
+    batterywarning.te \
+    bmm050d.te \
+    bmm056d.te \
+    boot_logo_updater.te \
+    br_app_data_service.te \
+    BGW.te \
+    ccci_fsd.te \
+    ccci_mdinit.te \
+    statusd.te \
+    flashlessd.te \
+    ccci_rpcd.te \
+    eemcs_fsd.te \
+    eemcs_mdinit.te \
+    dhcp6c.te \
+    dm_agent_binder.te \
+    dualmdlogger.te \
+    dumpstate.te \
+    em_svr.te \
+    enableswap.te \
+    factory.te \
+    fota1.te \
+    geomagneticd.te \
+    GoogleOtaBinder.te \
+    gsm0710muxdmd2.te \
+    gsm0710muxd.te \
+    guiext-server.te \
+    ipod.te \
+    matv.te \
+    mc6420d.te \
+    mdlogger.te \
+    mdnsd.te \
+    memsicd3416x.te \
+    memsicd.te \
+    meta_tst.te \
+    mmp.te \
+    mnld.te \
+    mobile_log_d.te \
+    mpud6050.te \
+    msensord.te \
+    mtk_6620_launcher.te \
+    mtk_agpsd.te \
+    mtkbt.te \
+    muxreport.te \
+    netdiag.te \
+    nvram_agent_binder.te \
+    nvram_daemon.te \
+    orientationd.te \
+    permission_check.te \
+    poad.te \
+    pppd_dt.te \
+    pppd_via.te \
+    pq.te \
+    recovery.te \
+    resmon.te \
+    mtkrild.te \
+    mtkrildmd2.te \
+    viarild.te \
+    s62xd.te \
+    sn.te \
+    epdg_wod.te \
+    ipsec.te \
+    terservice.te \
+    thermald.te \
+    thermal_manager.te \
+    thermal.te \
+    tiny_mkswap.te \
+    tiny_swapon.te \
+    vdc.te \
+    volte_imcb.te \
+    volte_ua.te \
+    volte_stack.te \
+    wmt_loader.te \
+    icusbd.te \
+    xlog.te \
+    mobicore.te \
+    install_recovery.te \
+    genfs_contexts
+
+BOARD_SEPOLICY_UNION += \
+	adbd.te \
+	bluetooth.te \
+	bootanim.te \
+	clatd.te \
+	debuggerd.te \
+	drmserver.te \
+	dhcp.te \
+	dnsmasq.te \
+	gpsd.te \
+	hci_attach.te \
+	healthd.te \
+	hostapd.te \
+	inputflinger.te \
+	init.te \
+	init_shell.te \
+	isolated_app.te \
+	keystore.te \
+	kernel.te \
+	lmkd.te \
+	logd.te \
+	mediaserver.te \
+	mtp.te \
+	nfc.te \
+	racoon.te \
+	radio.te \
+	rild.te \
+	runas.te \
+	sdcardd.te \
+	servicemanager.te \
+	shared_relro.te \
+	shell.te \
+	system_app.te \
+	system_server.te \
+	surfaceflinger.te \
+	tee.te \
+	ueventd.te \
+	uncrypt.te \
+	watchdogd.te \
+	wpa_supplicant.te \
+	wpa.te \
+	property.te \
+	property_contexts \
+	service.te \
+	dmlog.te \
+	MtkCodecService.te \
+	ppl_agent.te \
+	pvrsrvctl.te \
+	wifi2agps.te \
+	dex2oat.te \
+	emdlogger.te \
+	autokd.te \
+	ppp.te \
+	launchpppoe.te \
+	sbchk.te \
+	service_contexts \
+	ril-3gddaemon.te \
+	usbdongled.te \
+	zpppd_gprs.te \
+	md_ctrl.te \
+	cmddumper.te \
+	tunman.te
+
