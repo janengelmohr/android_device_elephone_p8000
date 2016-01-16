b1177475e5ed7599b8da99b4e84dd4dd30b58249
Dmitry Smirnov <divis1969@gmail.com>
2016-01-16 16:38:27 +0300

    Update binaries from 4.5.4I
    proprietary_files.txt is modified to match the stock image 4.5.4I (mymeizu.ru)
    
    Change-Id: Ia1deb088bc586327fc4a464e903ff65753261278

--- extract-files.sh
+++ extract-files.sh
@@ -2,8 +2,9 @@
 
 ## usage: extract-files.sh $1 $2
 ## $1 and $2 are optional
+## if $1 = image the files will be copied from the system image mounted at the folder specified by $2
 ## if $1 = unzip the files will be extracted from zip file (if $1 = anything else 'adb pull' will be used
-## $2 specifies the zip file to extract from (default = ../../../${DEVICE}_update.zip)
+## $2 specifies the folder where system image is mounted or zip file to extract from (default = ../../../${DEVICE}_update.zip)
 
 VENDOR=meizu
 DEVICE=meilan2
@@ -13,6 +14,8 @@ rm -rf $BASE/*
 
 if [ -z "$2" ]; then
     ZIPFILE=../../../${DEVICE}_update.zip
+elif [ "$1" = "image" ]; then
+    SRCDIR=$2
 else
     ZIPFILE=$2
 fi
@@ -27,6 +30,8 @@ else
 	fi
 	if [ "$1" = "unzip" ]; then
             unzip -j -o $ZIPFILE system/$FILE -d $BASE/$DIR
+	elif [ "$1" = "image" ]; then
+            cp $SRCDIR/$FILE $BASE/$FILE
 	else
             adb pull /system/$FILE $BASE/$FILE
 	fi
