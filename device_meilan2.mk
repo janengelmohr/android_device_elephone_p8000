476d6bda38f79d16821e75a088e6d0d9723847c9
Dmitry Smirnov <divis1969@gmail.com>
2016-01-30 02:14:37 +0300

    More changes for bring up
    - Keyboard config
    - Wifi config
    - Recovery fstab
    - init.*.rc
    
    Change-Id: I47eb7a7adf1ec1e06067a2e19bd5f8ae7216676d

--- device_meilan2.mk
+++ device_meilan2.mk
@@ -13,7 +13,10 @@ else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
+TARGET_PROVIDES_INIT_RC := true
+
 PRODUCT_COPY_FILES += \
+	device/meizu/meilan2/rootdir/init.rc:root/init.rc \
 	device/meizu/meilan2/rootdir/factory_init.rc:root/factory_init.rc \
 	device/meizu/meilan2/rootdir/factory_init.project.rc:root/factory_init.project.rc \
 	device/meizu/meilan2/rootdir/enableswap.sh:root/enableswap.sh \
@@ -39,6 +42,13 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
 	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
 	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml
+
+#PRODUCT_PACKAGES += \
+#    libion \
+#    libmockdrmcryptoplugin \
+#    libaudio-resampler \
+#    libtinycompress \
+#    libtinyxml
 
 # RIL
 PRODUCT_PACKAGES += \
@@ -75,9 +85,184 @@ persist.service.acm.enable=0 \
 persist.sys.usb.config=mtp \
 ro.mount.fs=EXT4 \
 debug.hwui.render_dirty_regions=false \
-ro.sf.lcd_density=320
+ro.sf.lcd_density=320 \
+ro.hardware.bluetooth=blueangel
 
 # Removed for now
 #ro.persist.partition.support=no \
 #ro.cip.partition.support=no \
 #persist.mtk.aee.aed=on \
+
+# Properties below are copied from device (build.prop)
+# but disabled for now
+#PRODUCT_PROPERTY_OVERRIDES := \
+#ro.com.android.dateformat=MM-dd-yyyy \
+#ro.config.ringtone=Technology.ogg \
+#ro.config.notification_sound=VideoRecord.ogg \
+#ro.config.mms_sound=Triumph.ogg \
+#ro.config.email_sound=VideoRecord.ogg \
+#ro.config.calendar_sound=Doorbell.ogg \
+#ro.config.alarm_alert=Morning_Dew.ogg \
+#ro.carrier=unknown \
+#ro.hwui.texture_cache_size=72 \
+#ro.hwui.layer_cache_size=50 \
+#ro.hwui.r_buffer_cache_size=8 \
+#ro.hwui.path_cache_size=32 \
+#ro.hwui.gradient_cache_size=3 \
+#ro.hwui.drop_shadow_cache_size=6 \
+#ro.hwui.fbo_cache_size=25 \
+#ro.hwui.texture_cache_flushrate=0.4 \
+#ro.hwui.text_small_cache_width=1024 \
+#ro.hwui.text_small_cache_height=1024 \
+#ro.hwui.text_large_cache_width=2048 \
+#ro.hwui.text_large_cache_height=1024 \
+#ro.bq.num_of_layer_used_by_sf=4 \
+#persist.sys.timezone=Asia/Shanghai \
+#persist.sys.meizu.region=cn \
+#persist.sys.meizu.codepage=gbk \
+#ro.meizu.setupwizard.flyme=true \
+#ro.meizu.setupwizard.setlang=true \
+#ro.meizu.region.enable=true \
+#ro.meizu.contactmsg.auth=false \
+#ro.meizu.customize.pccw=false \
+#ro.meizu.autorecorder=true \
+#ro.meizu.visualvoicemail=true \
+#ro.meizu.security=false \
+#ro.meizu.permanentkey=false \
+#ro.meizu.sip.support=true \
+#ro.meizu.voip.support=false \
+#sys.meizu.m35x.white.config=false \
+#sys.meizu.white.config=false \
+#persist.sys.log-main.enable=0 \
+#persist.sys.log-system.enable=0 \
+#persist.sys.log-events.enable=0 \
+#persist.sys.log-radio.enable=0 \
+#persist.sys.use.flyme.icon=true \
+#ro.adb.secure=1 \
+#keyguard.no_require_sim=true \
+#persist.sys.disable_blur_view=true \
+#persist.sys.static_blur_mode=false \
+#ro.debuggable=1 \
+#ro.operator.optr=CUST \
+#qemu.hw.mainkeys=1 \
+#dalvik.vm.heapgrowthlimit=128m \
+#dalvik.vm.heapsize=256m \
+#ro.mediatek.chip_ver=S01 \
+#ro.mediatek.version.release=ALPS.L1.MP3.V2_GINR6735.65C.L1_P48 \
+#ro.mediatek.platform=MT6735 \
+#ro.telephony.sim.count=2 \
+#persist.radio.default.sim=0 \
+#persist.radio.multisim.config=dsds \
+#persist.md.perm.checked=to_upgrade \
+#persist.gemini.sim_num=2 \
+#ro.gemini.smart_sim_switch=false \
+#ril.specific.sm_cause=0 \
+#bgw.current3gband=0 \
+#ril.external.md=0 \
+#ro.mtk_cam_lomo_support=1 \
+#ro.btstack=blueangel \
+#ro.sf.hwrotation=0 \
+#ril.current.share_modem=2 \
+#curlockscreen=1 \
+#ro.mediatek.gemini_support=true \
+#persist.radio.fd.counter=15 \
+#persist.radio.fd.off.counter=5 \
+#persist.radio.fd.r8.counter=15 \
+#persist.radio.fd.off.r8.counter=5 \
+#drm.service.enabled=true \
+#fmradio.driver.enable=1 \
+#ril.first.md=1 \
+#ril.flightmode.poweroffMD=1 \
+#ril.telephony.mode=0 \
+#dalvik.vm.mtk-stack-trace-file=/data/anr/mtk_traces.txt \
+#mediatek.wlan.chip=CONSYS_MT6735 \
+#mediatek.wlan.module.postfix=_consys_mt6735 \
+#ril.read.imsi=1 \
+#ril.radiooff.poweroffMD=0 \
+#ro.frp.pst=/dev/block/platform/mtk-msdc.0/by-name/frp \
+#ro.mediatek.version.branch=L1.MP3 \
+#ro.mediatek.version.sdk=4 \
+#ro.mtk_gemini_support=1 \
+#ro.mtk_audenh_support=1 \
+#ro.mtk_lossless_bt_audio=1 \
+#ro.mtk_besloudness_support=1 \
+#ro.mtk_bessurround_support=1 \
+#ro.mtk_gemini_enhancement=1 \
+#ro.mtk_wapi_support=1 \
+#ro.mtk_bt_support=1 \
+#ro.mtk_wappush_support=1 \
+#ro.mtk_agps_app=1 \
+#ro.mtk_wlan_support=1 \
+#ro.mtk_gps_support=1 \
+#ro.mtk_omacp_support=1 \
+#ro.mtk_search_db_support=1 \
+#ro.mtk_dialer_search_support=1 \
+#ro.mtk_dhcpv6c_wifi=1 \
+#ro.have_aacencode_feature=1 \
+#ro.mtk_fd_support=1 \
+#ro.mtk_oma_drm_support=1 \
+#ro.mtk_cta_drm_support=1 \
+#ro.mtk_widevine_drm_l3_support=1 \
+#ro.mtk_eap_sim_aka=1 \
+#ro.mtk_fm_recording_support=1 \
+#ro.mtk_audio_ape_support=1 \
+#ro.mtk_flv_playback_support=1 \
+#ro.mtk_wmv_playback_support=1 \
+#ro.mtk_send_rr_support=1 \
+#ro.mtk_emmc_support=1 \
+#ro.mtk_tetheringipv6_support=1 \
+#ro.mtk_phone_number_geo=1 \
+#ro.mtk_shared_sdcard=1 \
+#ro.mtk_enable_md1=1 \
+#ro.mtk_flight_mode_power_off_md=1 \
+#ro.mtk_pq_support=2 \
+#ro.mtk_miravision_support=1 \
+#ro.mtk_miravision_image_dc=1 \
+#ro.mtk_wfd_support=1 \
+#ro.mtk_wfd_sink_support=1 \
+#ro.mtk_wfd_sink_uibc_support=1 \
+#ro.mtk_sim_hot_swap=1 \
+#ro.mtk_thumbnail_play_support=1 \
+#ro.mtk_bip_scws=1 \
+#ro.mtk_cmcc_ft_precheck_support=1 \
+#ro.mtk_world_phone=1 \
+#ro.mtk_world_phone_policy=0 \
+#ro.mtk_perfservice_support=1 \
+#ro.mtk_sim_hot_swap_common_slot=1 \
+#ro.mtk_cta_set=1 \
+#ro.mtk_mobile_management=1 \
+#ro.mtk_antibricking_level=2 \
+#ro.mtk_cam_mfb_support=3 \
+#ro.mtk_slow_motion_support=1 \
+#ro.mtk_lte_support=1 \
+#ro.mtk_cam_mav_support=1 \
+#ro.mtk_cam_vfb=1 \
+#ro.mtk_rild_read_imsi=1 \
+#ro.sim_refresh_reset_by_modem=1 \
+#ro.mtk_live_photo_support=1 \
+#ro.mtk_motion_track_support=1 \
+#ro.mtk_passpoint_r1_support=1 \
+#ro.mtk_privacy_protection_lock=1 \
+#ro.mtk_bg_power_saving_support=1 \
+#ro.mtk_bg_power_saving_ui=1 \
+#ro.have_aee_feature=1 \
+#ro.sim_me_lock_mode=0 \
+#wfd.dummy.enable=1 \
+#ro.mediatek.project.path=device/ginreen/ginr6735_65c_l1 \
+#persist.mtk.wcn.combo.chipid=-1 \
+#service.wcn.driver.ready=no \
+#ro.wind_project_definition=5 \
+#ro.wind.pl.version=02 \
+#ro.wind_project_feature=1 \
+#ro.com.android.mobiledata=false \
+#persist.radio.mobile.data=0,0 \
+#persist.radio.mobile.enable=1,1 \
+#persist.meta.dumpdata=0 \
+#persist.sys.dalvik.vm.lib.2=libart.so \
+#dalvik.vm.isa.arm64.features=default \
+#dalvik.vm.isa.arm.features=default \
+#dalvik.vm.dexopt-flags=m=y \
+#net.bt.name=Android \
+#dalvik.vm.stack-trace-file=/data/anr/traces.txt \
+
+
