31153e9ac4baac1173a1de54bf5d24c22792d83a
Jan Engelmohr <jan.engelmohr@mailbox.tu-dresden.de>
2016-02-26 21:24:36 +0100

    added 64 bit support
    Change-Id: If0ea93134f51d5cfd998a4b35ba9e9681b6fb974

--- device_p8000.mk
+++ device_p8000.mk
@@ -1,3 +1,4 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
