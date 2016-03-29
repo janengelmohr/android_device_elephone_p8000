fe44a5b23d774c562f806fbd0bb739c7531fe890
walter79 <gzwalter79@gmail.com>
2016-03-29 20:36:44 +0200

    TWRP config update

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -339,44 +339,33 @@ BOARD_SEPOLICY_UNION += \
 RECOVERY_VARIANT := twrp
 
 TW_THEME := portrait_hdpi
-
 # brightness settings (needs verification)
 TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness/
 TW_MAX_BRIGHTNESS := 255
-
-
 # may be useful if we get graphical glitches
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
-
 # in case of wrong color this needs modification
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-
 # if sdcard0 is a /data/media emulated one
 RECOVERY_SDCARD_ON_DATA := true
-
 # ntfs support? (needs much space..)
 TW_INCLUDE_NTFS_3G := true
-
 # we may need that if sdcard0 dont work
 TW_FLASH_FROM_STORAGE := true
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_DEFAULT_EXTERNAL_STORAGE := true
-
 # name backup folders 'p8000' and not after MTK's fake hardware ID '1234567...'
 TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
-
 # we have it and it's enforcing!
 TWHAVE_SELINUX := true
-
 #only add if kernel supports
 #TW_INCLUDE_FUSE_EXFAT := true
-
 #F2FS support (only activate if kernel supports)
 #TARGET_USERIMAGES_USE_F2FS:=true
-
 # encryption
-TW_INCLUDE_JB_CRYPTO := false
-TW_INCLUDE_L_CRYPTO := true
-
-
+TW_INCLUDE_CRYPTO := true
+# Antiforensic wipe
+BOARD_SUPPRESS_SECURE_ERASE :=  true
+# CPU temp
+# TW_CUSTOM_CPU_TEMP_PATH := XXXXXX ?????
