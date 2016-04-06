13f6d1e888aba83fd0ab46b492360ea382703538
Jan Engelmohr <jan.engelmohr@mailbox.tu-dresden.de>
2016-04-06 09:40:56 +0200

    fixed typo in thermals
    Change-Id: I403e7475f23072ed4e6db1ff35d82d77b42eed31

--- device_p8000.mk
+++ device_p8000.mk
@@ -129,7 +129,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     	$(LOCAL_PATH)/configs/thermal/thermal.conf:system/etc/.tp/thermal.conf \
     	$(LOCAL_PATH)/configs/thermal/thermal.off.conf:system/etc/.tp/thermal.off.conf \
-    	$(LOCAL_PATH)/configs/thermal/.ht120.conf:system/etc/.tp/.h120.conf \
+    	$(LOCAL_PATH)/configs/thermal/.ht120.mtc:system/etc/.tp/.ht120.mtc \
     	$(LOCAL_PATH)/configs/thermal/.thermal_policy_00:system/etc/.tp/.thermal_policy_00
 
 # Charger
