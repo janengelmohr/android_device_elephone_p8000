26ecda5e35fe8c26cd718efa4bca8465bdadeda2
Jan Engelmohr <jan.engelmohr@mailbox.tu-dresden.de>
2016-05-02 15:41:01 +0200

    added tap2wake file, this needs a .te file assosiated and DT2W enabled in-kernel
    Change-Id: I720c2c1bf6dfd3e0d4ee78ec5fb423c9ea6b0b61

---
+++ cmhw/org/cyanogenmod/hardware/TapToWake.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2014 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cyanogenmod.hardware;
+
+import org.cyanogenmod.hardware.util.FileUtils;
+
+import java.io.DataOutputStream;
+
+import android.widget.Toast;
+
+import java.io.IOException;
+
+/**
+ * Tap (usually double-tap) to wake. This *should always* be supported by
+ * the hardware directly. A lot of recent touch controllers have a firmware
+ * option for this
+ */
+public class TapToWake {
+
+    private static String CONTROL_PATH = "/sys/android_touch/doubletap2wake";
+
+    /**
+     * Whether device supports it
+     *
+     * @return boolean Supported devices must return always true
+     */
+    public static boolean isSupported() {
+        return true;
+    }
+
+    /**
+     * This method return the current activation state
+     *
+     * @return boolean Must be false when feature is not supported or
+     * disabled.
+     */
+    public static boolean isEnabled() {
+        return (FileUtils.readOneLine(CONTROL_PATH).equals("1"));
+    }
+
+    /**
+     * This method allows to set activation state
+     *
+     * @param state The new state
+     * @return boolean for on/off, exception if unsupported
+     */
+    public static boolean setEnabled(final boolean state) {
+	DataOutputStream os;
+	Process suProcess;
+	try {
+	    suProcess = Runtime.getRuntime().exec("su");
+            os = new DataOutputStream(suProcess.getOutputStream());
+        	if(state) 
+		{
+                        os.writeBytes("echo 1 > /sys/android_touch/doubletap2wake\n");
+		}
+                else 
+		{
+                        os.writeBytes("echo 0 > /sys/android_touch/doubletap2wake\n");
+                }
+	os.flush();
+	return state;
+	} catch (IOException exception) 
+		{
+            exception.printStackTrace();
+	    return false;
+		}
+    }
+
+}
