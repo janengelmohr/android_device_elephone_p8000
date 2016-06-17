0ded069655a90fe16572ee493d964e237dcbd668
Jan Engelmohr <jan.engelmohr@mailbox.tu-dresden.de>
2016-06-17 15:54:51 +0200

    [testing] full.mk -> aosp_arm64.mk
    Change-Id: I17c0b90eac7b98859c0b01bfc239577c5e5469c8

--- device_p8000.mk
+++ device_p8000.mk
@@ -159,7 +159,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     	dalvik.vm.dex2oat-threads=2 \
     	dalvik.vm.image-dex2oat-threads=4
 
-$(call inherit-product, build/target/product/full.mk)
+#$(call inherit-product, build/target/product/full.mk)
+$(call inherit-product, build/target/product/aosp_arm64.mk)
 
 ADDITIONAL_DEFAULT_PROPERTIES += \
 	ro.secure=0 \
