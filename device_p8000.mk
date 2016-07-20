55f591de5277f625bc22b33c7b0ca1e05eab33bf
Jan Engelmohr <jan.engelmohr@mailbox.tu-dresden.de>
2016-07-20 21:32:58 +0200

    added ANT support
    Change-Id: I90c0734f0be3a491bfb8821c78e33ee836bd41f8

--- device_p8000.mk
+++ device_p8000.mk
@@ -92,6 +92,19 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	libbt-vendor
 
+# ANT
+PRODUCT_PACKAGES += \
+	libantradio \
+	antradio_app \
+	ANT_RAM_CODE_E1.BIN \
+	ANT_RAM_CODE_E2.BIN \
+	AntHalService \
+	com.dsi.ant.antradio_library
+
+# ANT Permissions
+PRODUCT_COPY_FILES += \
+	external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
+
 # Wifi
 PRODUCT_PACKAGES += \
     	lib_driver_cmd_mt66xx \
