7d86dc0106228bf34ff4c6bc2c7144fbeb2675de
Jan Engelmohr <jan.engelmohr@mailbox.tu-dresden.de>
2016-07-27 14:18:55 +0200

    add SIP permission
    Change-Id: I5bfedaedc53eb94020232e65302e108e0c11f43d

--- device_p8000.mk
+++ device_p8000.mk
@@ -60,6 +60,8 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
 	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
 	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+        frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+        frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
 	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/permissions/media_codecs.xml \
 	$(LOCAL_PATH)/configs/platform.xml:system/etc/permissions/platform.xml
 
