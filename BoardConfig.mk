c48699f3a7d39a89b7f968fd9417f6eee0895c95
Jan Engelmohr <jan.engelmohr@mailbox.tu-dresden.de>
2016-08-16 00:30:45 +0200

    use architecture specifics to common tree
    Change-Id: I52aadbb41d4c80792c96ab23cda9f00dbb3d8a5e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,44 +9,6 @@ TARGET_LDPRELOAD += libxlog.so
  
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := Auxus_PRIME_201
-TARGET_NO_BOOTLOADER := true
- 
-# 1st architecture aarch64
-TARGET_BOARD_PLATFORM := mt6753
-TARGET_ARCH := arm64
-TARGET_NO_BOOTLOADER := true
-TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
-TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_VARIANT := generic
-TARGET_BOARD_SUFFIX := _64
-TARGET_USES_64_BIT_BINDER := true
-TARGET_IS_64_BIT := true
-TARGET_CPU_CORTEX_A53 := true
-# 2nd architecture arm
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
-
-# Architecture Extensions
-ARCH_ARM_HAVE_NEON := true
-ARCH_ARM_HAVE_VFP := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
-TARGET_CPU_SMP := true
-
-# ABI lists for build.prop
-TARGET_CPU_ABI_LIST_64_BIT := $(TARGET_CPU_ABI)
-TARGET_CPU_ABI_LIST_32_BIT := $(TARGET_2ND_CPU_ABI),$(TARGET_2ND_CPU_ABI2)
-TARGET_CPU_ABI_LIST := $(TARGET_CPU_ABI_LIST_64_BIT),$(TARGET_CPU_ABI_LIST_32_BIT)
-
-# CFlags
-TARGET_GLOBAL_CFLAGS   += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
-COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
-TARGET_USERIMAGES_USE_EXT4 := true
 
 # needed for mass storage mode
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
