da741d959c75217284a92b34fa17a189087946f7
Jan Engelmohr <jan.engelmohr@mailbox.tu-dresden.de>
2016-08-16 00:40:49 +0200

    inherit MT6753 common tree and remove duplicates
    Change-Id: I679c36d780035035158239e1945bab3434b55461

--- device_p8000.mk
+++ device_p8000.mk
@@ -1,10 +1,4 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
-
+$(call inherit-product, device/mediatek/mt6753_common/device_mt6753.mk)
 $(call inherit-product, vendor/elephone/p8000/p8000-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/elephone/p8000/overlay
@@ -43,158 +37,14 @@ PRODUCT_COPY_FILES += \
 # TWRP thanks to Hanuma50
 PRODUCT_COPY_FILES += device/elephone/p8000/recovery/twrp.fstab:recovery/root/etc/twrp.fstab
 
-
-# hardware specifics
-PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
-	frameworks/native/data/etc/android.hardware.faketouch.xml:system/etc/permissions/android.hardware.faketouch.xml \
-	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-	frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
-	frameworks/native/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
-	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-        frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-        frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
-	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/permissions/media_codecs.xml \
-	$(LOCAL_PATH)/configs/platform.xml:system/etc/permissions/platform.xml
-
-# CM's Snap camera
-PRODUCT_PACKAGES += \
-	Snap
-
-# MTK's Engineer Mode
-PRODUCT_PACKAGES += \
-        EngineerMode
-
-# MTK's XLog needed for Engineer Mode
-PRODUCT_PACKAGES += \
- 	libxlog
-
 # Fingerprint support
 PRODUCT_PACKAGES += fp
 PRODUCT_PACKAGES += slfpcal
 PRODUCT_PACKAGES += libslfpjni
 PRODUCT_PACKAGES += libsileadinc_dev
 
-include device/elephone/p8000/MediaTek/Fingerprint/slfpcal/Android.mk
-include device/elephone/p8000/MediaTek/Fingerprint/fp/Android.mk
-
-# Gralloc
-PRODUCT_PACKAGES += \
-   	libgralloc_extra
-
-# Bluetooth
-PRODUCT_PACKAGES += \
-	libbt-vendor
-
-# ANT
-PRODUCT_PACKAGES += \
-	libantradio \
-	antradio_app \
-	ANT_RAM_CODE_E1.BIN \
-	ANT_RAM_CODE_E2.BIN \
-	AntHalService \
-	com.dsi.ant.antradio_library
-
-# ANT Permissions
-PRODUCT_COPY_FILES += \
-	external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
-
-# Wifi
-PRODUCT_PACKAGES += \
-    	lib_driver_cmd_mt66xx \
-    	libwifi-hal-mt66xx \
-    	wifi_hal \
-    	libwpa_client \
-    	hostapd \
-    	hostapd_cli \
-    	dhcpcd.conf \
-    	wpa_supplicant \
-    	wpa_supplicant.conf \
-	libnl_2
-   	
-# Audio
-PRODUCT_PACKAGES += \
-    	audio.a2dp.default \
-    	audio_policy.default \
-    	audio_policy.stub \
-    	audio.r_submix.default \
-    	audio.usb.default \
-    	libaudio-resampler \
-    	tinymix \
-    	libtinyalsa \
-    	libtinycompress \
-    	librs_jni \
-	libtinyxml
-
-# Audio profiles used to address the correct audio devices for headset, etc.
-PRODUCT_COPY_FILES += \
-    	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    	$(LOCAL_PATH)/configs/audio_device.xml:system/etc/audio_device.xml \
-    	$(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
-    	$(LOCAL_PATH)/configs/audio_effects.conf:system/etc/audio_effects.conf
-
-# Thermal
-PRODUCT_COPY_FILES += \
-    	$(LOCAL_PATH)/configs/thermal/thermal.conf:system/etc/.tp/thermal.conf \
-    	$(LOCAL_PATH)/configs/thermal/thermal.off.conf:system/etc/.tp/thermal.off.conf \
-    	$(LOCAL_PATH)/configs/thermal/.ht120.mtc:system/etc/.tp/.ht120.mtc \
-    	$(LOCAL_PATH)/configs/thermal/.thermal_policy_00:system/etc/.tp/.thermal_policy_00
-
-# Charger
-PRODUCT_PACKAGES += \
-      	charger_res_images
-
-# Live Display
-PRODUCT_PACKAGES += \
-	libjni_livedisplay
-
-# Telecom
-PRODUCT_COPY_FILES += \
-    	$(LOCAL_PATH)/configs/apns-conf.xml:system/etc/apns-conf.xml \
-    	$(LOCAL_PATH)/configs/ecc_list.xml:system/etc/ecc_list.xml \
-    	$(LOCAL_PATH)/configs/spn-conf.xml:system/etc/spn-conf.xml 
-
-# Torch
-PRODUCT_PACKAGES += \
-    	Torch
-
-# Network dependency
-PRODUCT_PACKAGES += \
-    	libifaddrs	
-
-# STk
-PRODUCT_PACKAGES += \
-    	Stk
-
-# GPS
-PRODUCT_COPY_FILES += \
-     	$(LOCAL_PATH)/configs/agps_profiles_conf2.xml:system/etc/agps_profiles_conf2.xml 
-
-# FM Radio
-PRODUCT_PACKAGES += \
-     	FmRadio \
-     	libfmjni \
-	libfmmt6620 \
-        libfmmt6628 \
-        libfmmt6627 \
-        libfmmt6630 \
-        libfmcust 
-
-# Media	
-PRODUCT_COPY_FILES += \
-    	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-	$(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+include device/elephone/p8000/Fingerprint/slfpcal/Android.mk
+include device/elephone/p8000/Fingerprint/fp/Android.mk
 
 
 PRODUCT_COPY_FILES += \
@@ -207,12 +57,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
     	dalvik.vm.image-dex2oat-threads=4
 
 $(call inherit-product, build/target/product/full.mk)
-
-ADDITIONAL_DEFAULT_PROPERTIES += \
-	ro.secure=0 \
-	ro.allow.mock.location=1 \
-	ro.debuggable=1 \
-	ro.adb.secure=1 \
-	persist.service.acm.enable=0 \
-	ro.oem_unlock_supported=1 \
-	persist.sys.usb.config=mtp
