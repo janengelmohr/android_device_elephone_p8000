69ae9c6d7b487c5e3c8110fbfa273b6e7e1c18a2
Jan Engelmohr <jan.engelmohr@mailbox.tu-dresden.de>
2016-08-16 00:33:39 +0200

    add multirom makefile [WIP]
    Change-Id: I203368dd75366feaa8ed86911635325c674f48c7

---
+++ multirom.mk
@@ -0,0 +1,8 @@
+# MultiROM config
+MR_INPUT_TYPE := type_b
+MR_INIT_DEVICES := device/elephone/p8000/multirom/mulirom_init_devices.c
+#reMR_RD_ADDR := 0x82500000
+MR_DPI := xhdpi
+MR_DPI_FONT := 340
+MR_FSTAB := device/elephone/p8000/recovery/twrp.fstab
+MR_KEXEC_MEM_MIN := 0x85000000
