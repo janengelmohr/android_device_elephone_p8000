58c4787257a73f4c64f93b007d9faafe53f94271
Jan Engelmohr <jan.engelmohr@mailbox.tu-dresden.de>
2016-09-27 19:52:55 +0200

    Revert "add missing symbol _ZN7android11IDumpTunnel11asInterfaceERKNS_2spINS_7IBinderEEE"
    This reverts commit f3b9e51ee2914d23f1c825de157562bf2b734ae2.

--- libmtk_symbols/mtk_gui.cpp
+++ libmtk_symbols/mtk_gui.cpp
@@ -6,6 +6,4 @@ extern "C" {
     void _ZN7android14SurfaceControl8setLayerEi(int32_t layer){
         _ZN7android14SurfaceControl8setLayerEj(static_cast<uint32_t>(layer));
     }
-
-    void _ZN7android11IDumpTunnel11asInterfaceERKNS_2spINS_7IBinderEEE(){}
 }
